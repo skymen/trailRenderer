9001bdbcad4a7852cd4b649d9114826675512cc3
Ossama <skymen75019@gmail.com>
2023-08-10 18:04:28 +0200

    initial commit

---
+++ src/instance.js
@@ -0,0 +1,36 @@
+function getInstanceJs(parentClass, scriptInterface, addonTriggers, C3) {
+  return class extends parentClass {
+    constructor(inst, properties) {
+      super(inst);
+
+      if (properties) {
+      }
+    }
+
+    Release() {
+      super.Release();
+    }
+
+    SaveToJson() {
+      return {
+        // data to be saved for savegames
+      };
+    }
+
+    LoadFromJson(o) {
+      // load state for savegames
+    }
+
+    Trigger(method) {
+      super.Trigger(method);
+      const addonTrigger = addonTriggers.find((x) => x.method === method);
+      if (addonTrigger) {
+        this.GetScriptInterface().dispatchEvent(new C3.Event(addonTrigger.id));
+      }
+    }
+
+    GetScriptInterfaceClass() {
+      return scriptInterface;
+    }
+  };
+}
