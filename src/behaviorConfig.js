ebd3a3bdfe640bae1c5c81103c020507b4860b1f
Ossama <skymen75019@gmail.com>
2023-08-24 01:19:46 +0200

    Updated icon + 2.0.0.9

--- src/behaviorConfig.js
+++ src/behaviorConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   addonType: "behavior",
   id: "skymenTrail",
   name: "Trail Renderer",
-  version: "2.0.0.8",
+  version: "2.0.0.9",
   category:
     // "attributes",
     // "movements",
